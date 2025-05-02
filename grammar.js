6183e5e805410c1f0c5544f053488c8362f0fcb9
Nathan <nathan.titirangi@gmail.com>
2025-05-03 10:53:36 +1200

    name fields for highlights

--- grammar.js
+++ grammar.js
@@ -8,7 +8,7 @@ module.exports = grammar({
 	rules: {
 		source_file: $ => repeat($.statement),
 
-		using: $ => seq("using", $.identifier, "=", $.arraylike_type, ";"),
+		using: $ => seq($.keyword_using, $.identifier, "=", $.arraylike_type, ";"),
 		assignment: $ => seq($.identifier, "=", $.expr),
 		generic_decl: $ => seq("<", sep1($.generic_decl_param, ","), ">"),
 		generic_decl_param: $ => choice($.identifier, seq("auto", $.identifier)),
@@ -16,17 +16,17 @@ module.exports = grammar({
 		generic_param: $ => choice($.expr, $.arraylike_type),
 		type: $ => seq(optional($.endianness), $.type_identifier, optional($.generic)),
 		arraylike_type: $ => seq($.type, repeat($.type_suffix)),
-		padding: $ => seq("padding", "[", $.expr, "]", ";"),
-		specialised_field: $ => seq($.identifier, optional($.type_suffix)),
+		padding: $ => seq($.keyword_padding, "[", $.expr, "]", ";"),
+		specialised_field: $ => seq(field("field_name", $.identifier), optional($.type_suffix)),
 		type_field: $ => seq($.type, sep1($.specialised_field, ","), ";"),
 		maybe_type_field: $ => choice($.padding, $.type_field),
 		type_suffix: $ => seq("[", $.expr, "]"),
-		struct_def: $ => seq("struct", $.identifier, optional($.generic_decl), "{", repeat($.maybe_type_field), "}"),
+		struct_def: $ => seq($.keyword_struct, field("struct_name", $.identifier), optional($.generic_decl), "{", repeat($.maybe_type_field), "}"),
 		signedness: $ => prec(1, choice("signed", "unsigned")),
 		bitfield_type: $ => choice($.signedness, $.type),
 		bitfield_type_field: $ => seq(optional($.bitfield_type), $.identifier, ":", $.integer, ";"),
 		bitfield_field: $ => choice($.bitfield_type_field, $.type_field),
-		bitfield_def: $ => seq("bitfield", $.identifier, optional($.generic_decl), "{", repeat($.bitfield_type_field), "}"),
+		bitfield_def: $ => seq($.keyword_bitfield, $.identifier, optional($.generic_decl), "{", repeat($.bitfield_type_field), "}"),
 		type_def: $ => seq(choice($.struct_def, $.bitfield_def), ";"),
 		expr_leaf: $ => prec(1, choice($.number, $.identifier)),
 		sub: $ => prec.left(1, seq($.expr, "-", $.expr)),
@@ -42,6 +42,11 @@ module.exports = grammar({
 		identifier: $ => /[a-zA-Z][\w-]*/,
 		multiline_comment: $ => /\/\*[.\n]*\*\//,
 		line_comment: $ => /\/\/.*\n/,
+
+		keyword_using: $ => "using",
+		keyword_padding: $ => "padding",
+		keyword_bitfield: $ => "bitfield",
+		keyword_struct: $ => "struct",
 	}
 });
 
